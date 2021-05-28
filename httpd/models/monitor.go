661dd410dbfac849daccdaadb56a822740a3647c
lij138 <lij138@bngrp.com>
2021-05-28 02:43:59 -0400

    tt

---
+++ httpd/models/monitor.go
@@ -0,0 +1,8 @@
+package models
+
+type MonitorCluster struct {
+	BaseModel
+	Code   string `gorm:"column:code;type:varchar(64);not null" json:"code" form:"code" binding:"required"`
+	Name   string `gorm:"column:name;type:varchar(64);not null" json:"name" form:"name" binding:"required"`
+	PrometheusUrl   string `gorm:"column:prometheusUrl;type:varchar(64);not null" json:"prometheusUrl" form:"prometheusUrl" binding:"required"`
+}
